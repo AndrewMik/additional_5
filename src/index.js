a386234cf6cb3c9f11f2641d752bb7989bd9a654
AndrewMik <Andrew.Mikitchuk@gmail.com>
2018-02-24 14:52:05 +0300

    Task Additional_5: Solution of the task 6

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+  var bracketsStack = [];
+
+  if (str.length % 2 != 0) {
+    return false;
+  }
+
+  for (var i = 0; i < str.length; i++) {
+    for (var j = 0; j < bracketsConfig.length; j++) {
+      if (bracketsStack[bracketsStack.length - 1] == bracketsConfig[j][0] && str[i] == bracketsConfig[j][1]) {
+        bracketsStack.pop();
+      } else {
+        if (str[i] == bracketsConfig[j][0]) {
+          bracketsStack.push(str[i]);
+        }
+      }
+    }
+  }
+  return (bracketsStack.length == 0) ? true : false;
 }
